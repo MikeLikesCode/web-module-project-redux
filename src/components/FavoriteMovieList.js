7daddd1b733c9cc78d8f7f30a6491a1c6472192e
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-08-10 23:28:41 -0400

    Initial commit, adding solution code

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { removeFavorite } from '../actions/favoritesActions'
 
 import { Link } from 'react-router-dom';
 
 
 const FavoriteMovieList = (props) => {
-    const favorites = [];
-    
+    console.log(props)
     return (<div className="col-xs savedContainer">
         <h5>Favorite Movies</h5>
         {
-            favorites.map(movie=>{
+            props.favorites.map(movie=>{
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
+                        <span onClick={() => props.removeFavorite(movie.id)}><span class="material-icons">remove_circle</span></span>
                     </Link> 
                 </div>
             })
@@ -21,5 +22,11 @@ const FavoriteMovieList = (props) => {
     </div>);
 }
 
+const mapStateToProps = (state) => {
+    state = state.favoritesReducer
+    return{
+        favorites: state.favorites
+    }
+}
 
-export default FavoriteMovieList;
+export default connect(mapStateToProps, { removeFavorite })(FavoriteMovieList);
